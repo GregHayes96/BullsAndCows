affbe2d37bfa32df2a574c62ed6b116b1e532255
Greg Hayes <gregphayea1@gmail.com>
2022-09-20 10:39:27 +0100

    Add project files.

---
+++ Practice/Program.cs
@@ -0,0 +1,29 @@
+﻿
+BullsAndCows game = new BullsAndCows (3456);
+
+
+class BullsAndCows
+{
+    int value;      //field  -  Data Member 
+    int guess;
+
+    public BullsAndCows(int num)        //constructor
+    {
+        value = num;
+    }
+
+    public int GetVal()         //method  -  Function Member
+    {
+        return value;
+    }
+
+    public void SetVal(int num)
+    {
+        value = num;
+    }
+
+    public void NewGuess(int num)
+    {
+        guess = num;
+    }
+}
